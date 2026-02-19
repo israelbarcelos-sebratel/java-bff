3257095fb8e72e63ebfdc3fae8e6c29b72eb6b71
israel.barcelos@sebratel.com.br <israel.barcelos@corp.sebratel.com.br>
2026-02-19 17:55:53 -0300

    Correção de endpoint de movimentação de estoque

--- src/main/java/br/com/sebratel/bff/controller/MovimentacaoEstoqueController.java
+++ src/main/java/br/com/sebratel/bff/controller/MovimentacaoEstoqueController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/estoque")
+@RequestMapping("/api/v1/estoque")
 public class MovimentacaoEstoqueController {
 
     private final MovimentacaoEstoqueService movimentacaoEstoqueService;
