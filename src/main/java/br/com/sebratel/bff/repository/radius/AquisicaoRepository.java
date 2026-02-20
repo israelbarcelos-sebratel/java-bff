636db40049a4493b505776f671593837b45ef895
israel.barcelos@sebratel.com.br <israel.barcelos@corp.sebratel.com.br>
2026-02-20 10:23:00 -0300

    correção da query de AquisicaoRepository

--- src/main/java/br/com/sebratel/bff/repository/radius/AquisicaoRepository.java
+++ src/main/java/br/com/sebratel/bff/repository/radius/AquisicaoRepository.java
@@ -12,33 +12,35 @@ import java.util.List;
 public interface AquisicaoRepository extends JpaRepository<RadiusContract, Long> {
 
     @Query(value = """
-        SELECT 
-            par.id AS id,
-            sp.code AS codigo,
-            sp.title AS produto,
-            par.date AS data,
-            p.name AS requisitadoPor,
-            par.receipt_date_prevision AS dataPrevisao,
-            pari.units AS unidades,
-            CASE 
-                WHEN UPPER(par.observation) LIKE '%SCHARLAU%' THEN 'BASE OPERACIONAL - SÃO LEOPOLDO'
-                WHEN UPPER(par.observation) LIKE '%VICTOR BARRETO%' THEN 'BASE OPERACIONAL - CANOAS'
-                WHEN UPPER(par.observation) LIKE '%NAVEGANTES%' THEN 'SEDE - PORTO ALEGRE'
-                ELSE 'NÃO IDENTIFICADA'
-            END AS base,
-            CASE 
-                WHEN par.status = 4 THEN 'Aguardando Entrega'
-                WHEN par.status = 2 THEN 'Em Aprovação'
-                WHEN par.status = 1 THEN 'Inclusão'
-            END AS status
-        FROM erp.product_acquisition_requests par
-        LEFT JOIN erp.product_acquisition_request_items AS pari ON pari.product_acquisition_request_id = par.id
-        LEFT JOIN erp.service_products sp ON sp.id = pari.service_product_id
-        LEFT JOIN erp.people p ON p.id = par.person_id
-        WHERE par.status IN (1, 2, 4) 
-          AND par.deleted IS FALSE
-        GROUP BY par.id, sp.code, sp.title, par.date, p.name, par.receipt_date_prevision, pari.units
-        ORDER BY par.date DESC
-        """, nativeQuery = true)
+            
+                SELECT
+                    par.id,
+                    sp.code AS "CÓDIGO",
+                    sp.title AS "PRODUTO",
+                    par.date AS "DATA",
+                    p.name AS "REQUISITADO POR",
+                    par.receipt_date_prevision AS "DATA PREVISÃO",
+                    pari.units AS "OUTROS STATUS",
+                        CASE\s
+                            WHEN UPPER(par.observation) LIKE '%SCHARLAU%' THEN 'BASE OPERACIONAL - SÃO LEOPOLDO'
+                            WHEN UPPER(par.observation) LIKE '%VICTOR BARRETO%' THEN 'BASE OPERACIONAL - CANOAS'
+                            WHEN UPPER(par.observation) LIKE '%NAVEGANTES%' THEN 'SEDE - PORTO ALEGRE'
+                        END AS "BASE",
+                        CASE\s
+                            WHEN par.status = 4 THEN 'Aguardando Entrega'
+                            WHEN par.status = 2 THEN 'Em Aprovação'
+                            WHEN par.status = 1 THEN 'Inclusão'
+                        END AS "STATUS"
+                FROM product_acquisition_requests par
+                LEFT JOIN product_acquisition_request_items AS pari ON pari.product_acquisition_request_id = par.id
+                LEFT JOIN service_products sp ON sp.id = pari.service_product_id
+                LEFT JOIN people p ON p.id = par.person_id
+                LEFT JOIN company_place_business_units cpbu ON cpbu.company_place_id = par.company_place_id\s
+                LEFT JOIN units_measures um ON um.code = sp.code
+                WHERE par.status in (1, 2, 4)\s
+                AND par.deleted IS FALSE
+                GROUP BY par.id, sp.code, sp.title, par.date, p.name, par.receipt_date_prevision, pari.units
+                ORDER BY par.date desc
+            """, nativeQuery = true)
     List<AquisicaoProjection> findAquisicoesPendentes();
 }
